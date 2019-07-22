a1683848914bf05fde8b2e8f6a0b6f1fd5ff96ed
Catherine Weinberg <catherinewweinberg@gmail.com>
2019-07-22 19:11:24 -0400

    added formatting to survey.html - about you section, placeholders for questions 1-5, bootstrap form/choices to questions, submit button, and fontawesome icons.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,9 @@
 var friends = require("../data/friends.js");
-var path = require("path");
-var fs = require("fs");
 
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
+
+  app.post("/api/friends", function(req, res) {});
 };
