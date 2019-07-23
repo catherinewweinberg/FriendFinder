18726837ebbc30db1942eea310aec04ee655ae29
Catherine Weinberg <catherinewweinberg@gmail.com>
2019-07-22 20:47:10 -0400

    defined variables in apiroutes.js and added jquery to survey.html

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+var friends = [
+  {
+    name: "Jane Doe",
+    photo: needtolinkapicture.jpg,
+    surveyQuestions: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+  },
+  {
+    name: "John Smith",
+    photo: needtolinkapicture.jpg,
+    surveyQuestions: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+  }
+];
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,5 +5,23 @@ module.exports = function(app) {
     res.json(friends);
   });
 
-  app.post("/api/friends", function(req, res) {});
+  app.post("/api/friends", function(req, res) {
+    var newFriend = req.body;
+    var newScore = 0;
+    var total = 0;
+    var match = {
+      name: "",
+      photo: "",
+      difference: 1000
+    };
+
+    // calculating the total is not complete
+    for (var i = 0; i < friends.length; i++) {
+      total = 0;
+    }
+
+    friends.push(newFriend);
+    res.json(match);
+    console.log(match);
+  });
 };
